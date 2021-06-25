862b0c0d002ae3fca6193f2ced4a52eddf6ce41f
JaeminKang <kangjm2@hanyang.ac.kr>
2021-06-25 10:57:19 +0900

    FIX : #2 Fix Route Configuration

--- src/main/java/kr/meet42/apigatewayservice/filter/AuthorizationFilter.java
+++ src/main/java/kr/meet42/apigatewayservice/filter/AuthorizationFilter.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
@@ -15,11 +14,12 @@ import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
-public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config>
+public class AuthorizationFilter extends AbstractGatewayFilterFactory<AuthorizationFilter.Config>
 {
     private final Environment env;
+    private static String ACCESS_TOKEN = "access-token";
 
-    public AuthorizationHeaderFilter(Environment env) {
+    public AuthorizationFilter(Environment env) {
         super(Config.class);
         this.env = env;
     }
@@ -34,11 +34,15 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
         return (exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
 
-            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)){
+            // refresh token이 있다면? -> 재발급 로직 태워야 함
+            // member에서 refresh해줄거고
+            // 이걸 어떻게 심어서 원 경로에다 보내지?
+
+            if (!request.getHeaders().containsKey(ACCESS_TOKEN)){
                 return onError(exchange, "No authorization header", HttpStatus.UNAUTHORIZED);
             }
 
-            String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String authorizationHeader = request.getHeaders().get(ACCESS_TOKEN).get(0);
             String jwt = authorizationHeader.replace("Bearer", "");
 
             if (!isJwtValid(jwt)) {
@@ -57,6 +61,8 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
         try {
             subject = Jwts.parser().setSigningKey(env.getProperty("token.secret"))
                     .parseClaimsJws(jwt).getBody().getSubject();
+
+
         } catch (Exception ex) {
             returnValue = false;
         }
@@ -71,7 +77,6 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
     private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
         ServerHttpResponse response = exchange.getResponse();
         response.setStatusCode(httpStatus);
-
         log.error(err);
         return response.setComplete();
     }
